013192be84019450766dc258d543c5d01055630a
AdamRh <122239375+JamfCoder@users.noreply.github.com>
2023-08-29 22:30:11 -0500

    add
    add

--- DeployLab-to-Azure-Labs/templates.tf
+++ DeployLab-to-Azure-Labs/templates.tf
@@ -45,3 +45,62 @@ resource "azurerm_kubernetes_cluster" "azurecilium" {
     ebpf_data_plane     = "cilium"
   }
 }
+
+// Create an SQL Server
+resource "azurerm_sql_server" "example" {
+  name                         = "demo-mssql-server"
+  resource_group_name          = data.azurerm_resource_group.example.name
+  location                     = data.azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "adminuser"
+  administrator_login_password = "password"
+}
+
+// Create a database in the SQL Server
+
+resource "azurerm_sql_database" "example" {
+  name                = "demo-mssql-database"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  edition             = "Standard"
+  collation           = "SQL_Latin1_General_CP1_CI_AS"
+  requested_service_objective_name = "S0"
+}
+
+resource "azurerm_sql_firewall_rule" "example" {
+  name                = "FirewallRule1"
+  resource_group_name = data.azurerm_resource_group.example.name
+  server_name         = azurerm_sql_server.example.name
+  start_ip_address    = "IP_ADDRESS_TO_ALLOW"
+  end_ip_address      = "IP_ADDRESS_TO_ALLOW"
+}
+// create app service 
+
+resource "azurerm_app_service_plan" "appserviceplan" {
+    name                = "demo-app-service-plan"
+    location            = data.azurerm_resource_group.example.location
+    resource_group_name = data.azurerm_resource_group.example.name
+    kind                = "Windows"
+    reserved            = false
+  
+    sku {
+      tier = "Standard"
+      size = "S1"
+    }
+}
+  
+
+resource "azurerm_app_service" "appservice" {
+    name                = "demo-bookstoreapi"
+    location            = data.azurerm_resource_group.example.location
+    resource_group_name = data.azurerm_resource_group.example.name
+    app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+    connection_string {
+    name  = "demo-mssql-database"
+    type  = "SQLServer"
+    value = "serverconnectionstring" 
+  }
+  
+}
